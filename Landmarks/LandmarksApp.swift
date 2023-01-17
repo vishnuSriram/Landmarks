fdc7bb39e9e79026afae0f5bdc6be992c66af070
Vishnu Sriram <vsriram42@gmail.com>
2023-01-17 17:45:19 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vishnu Sriram on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
